1bd5842bd1b13b0909fef501479bda829e02db9f
HADDAD Moez <mhaddad@ats-digital.com>
2021-02-17 20:04:08 +0100

    feat: preparation for api calls

---
+++ src/constants/index.js
@@ -0,0 +1,1 @@
+export const { REACT_APP_HTTP_LINK } = process.env;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { REACT_APP_HTTP_LINK } from './constants';
+
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: REACT_APP_HTTP_LINK,
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
